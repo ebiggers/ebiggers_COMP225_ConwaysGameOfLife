f37ae7b2a3aa634b41192b0c006f2442ba76131d
Eric Biggers <ebiggers3@gmail.com>
2012-09-20 13:39:07 -0500

    Remove generated files

--- GameOfLife/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/e/comp/225/cmdline/GameOfLife/bin/AndroidManifest.xml : \
-/home/e/comp/225/cmdline/GameOfLife/AndroidManifest.xml \
--- GameOfLife/bin/GameOfLife-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/home/e/comp/225/cmdline/GameOfLife/bin/GameOfLife-debug-unaligned.apk : \
-/home/e/comp/225/cmdline/GameOfLife/bin/GameOfLife.ap_ \
-/home/e/comp/225/cmdline/GameOfLife/bin/classes.dex \
-/opt/android-sdk/tools/support/annotations.jar \
